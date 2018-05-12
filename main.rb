a1ff7ad8fd6e890b512c833c899cdcf42ee3665f
shiotomo <t.shiota16@gmail.com>
2018-05-12 05:55:14 +0000

    コメントを追加した

--- main.rb
+++ main.rb
@@ -1,16 +1,23 @@
+# Ayataka
+
+# 実行時引数が無いときプログラムを終了する
 exit if ARGV == []
 
+# 実行時引数をarrに格納する
 arr = ARGV
+
+# 実行時引数と同じ数の配列を用意する
 num = Array.new(arr.length, 0)
 
+# 100000回ループを発生させる
 100000.times do
   num[rand(arr.length)] += 1
 end
 
+# 実行結果を表示
 result = arr[num.index(num.max)]
 
-puts "選ばれたのは#{result}でした"
-puts ""
+print "選ばれたのは#{result}でした\n\n"
 puts "== 結果内容 =="
 p arr
 p num
